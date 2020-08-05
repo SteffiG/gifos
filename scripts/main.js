8a2c29bb277becd92ecd74f3e8509c624b24482a
SteffiG <gutierrezsteffi@gmail.com>
2020-08-05 16:23:39 -0500

    darkmode

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,23 @@ window.onload = function(){
     })
 }
 
+//-----------------------------------------------------------------
+//DARK MODE
+const blackout = document.querySelector(".menuInner-link_style");
+blackout.addEventListener("click", changeMode);
+
+/**
+* Funcion para el efecto modo oscuro
+* la variable change llama a el cuerpo de la pagina
+* luego le paso a esa variable la propiedad classList y el metodo toggle 
+* para llamar a la clase css que contiene las propiedades que cambian el color de fondo
+*/ 
+function changeMode() {
+    //alert("funciona");
+    let change = document.body;
+    change.classList.toggle("oscuro");
+}
+
 //------------------------------------------------------------------
 //SEARCH GIF
 const searchGif = document.querySelector(".search-input");
@@ -32,12 +49,6 @@ function search() {
         console.log(this.value);
         return response.json();
     })
-    /*.then(() => {
-        let name = document.querySelector("searchGif");
-        let word = document.createElement('p');
-        name.appendChild(word);
-        name.innerHTML = this.value;
-    })*/
     .then((data) => {
         console.log(data);
         let imagesGif = data.data;
@@ -53,69 +64,3 @@ function search() {
 
     .catch((error) => reject(error))
 }
-
-/*const searchWord = document.querySelector(".searchName");
-searchWord.addEventListener('change', word);
-
-function word() {
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=A1hJOpkrFlJITK2YiwMHoqqnOKdoKKYs&q=${this.value}&limit=12&rating=g`)
-}*/
-
-//-----------------------------------------------------------------
-//BACKGROUND GIFOS 
-/*
-const backgroundChange = document.querySelector(".searchGif-img");
-console.log(backgroundChange);
-backgroundChange.addEventListener("mouseover", backgroundColor);
-function backgroundColor(event){
-    event.target.style.background = "rgba(87, 46, 229, 0.6);";
-}
-*/
-//-----------------------------------------------------------------
-//DARK MODE
-const blackout = document.querySelector(".menuInner-link_style");
-blackout.addEventListener("click", changeMode);
-
-/**
-* Funcion para el efecto modo oscuro
-* la variable change llama a el cuerpo de la pagina
-* luego le paso a esa variable la propiedad classList y el metodo toggle 
-* para llamar a la clase css que contiene las propiedades que cambian el color de fondo
-*/ 
-function changeMode(event) {
-    let change = document.body;
-    change.classList.toggle("oscuro");
-    if (event.value === "Modo Nocturno") {
-        event.value = "Modo Diurno";
-      }
-    else {
-        event.target.value = "Modo Nocturno";
-      }
-    //document.querySelector(".oscuro").textContent = "Modo Diurno";
-}
-
-//-----------------------------------------------------------------
-
-/**
- * 
- * @method: searchTrending
- * @description: funcion para mostrar las palabras mas busquedas ultimamente
- * @return: Retorna las busquedas mas destacadas de gifos
- * 
- */
-
-const searchPhrases = document.querySelector(".paragraph");
-searchPhrases.addEventListener("load",searchTrending);
-
-/*
-const searchTrending = (() => {
-    return new Promise((resolve, reject) => {
-        fetch('https://api.giphy.com/v1/trending/searches?api_key=A1hJOpkrFlJITK2YiwMHoqqnOKdoKKYs')
-        .then((response) => {
-            console.log(response);
-            resolve (response.json())
-        }
-        .catch((error) => reject(error))
-    }) 
-})
-*/
