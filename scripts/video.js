2f2cd9852d568cf48c8ad897e55e63d5ccf80580
Steffi Gutierrez <gutierrezsteffi@gmail.com>
2020-09-18 14:39:33 -0500

    creating gifo

--- scripts/video.js
+++ scripts/video.js
@@ -3,21 +3,25 @@
  */
 const video = document.querySelector('.video')
 const start = document.querySelector('.start');
-const record = document.querySelector('record');
+const record = document.querySelector('.record');
 const end = document.querySelector('.finish');
 const up = document.querySelector('.upGifo');
+const circle = document.getElementsByClassName('creating__options--circle');
 const title = document.querySelector('.creating__written--title');
 const paragraph = document.querySelector('.creating__written--paragraph');
+const timer = document.querySelector('.creating__options--timer');
+const repeat = document.querySelector('.creating__options--repeat');
 let constraints = { audio: false,  video: {width: 480,height: 320 }}
+let initTime, idInterval;
 
 /**
  * @method startVideo
  * @description - function in charge of listening to the click event of the start button
  */
 
- function startVideo() {
-    cameraPermissions();
-    getStreamAndRecord();
+function startVideo() {
+  cameraPermissions();
+  getStreamAndRecord();
 }
 
 /**
@@ -26,9 +30,10 @@ let constraints = { audio: false,  video: {width: 480,height: 320 }}
  */
 
 function cameraPermissions() {
-    title.innerHTML = '¿Nos das acceso a tu cámara?';
-    paragraph.innerHTML = 'El acceso a tu camara será válido sólo por el tiempo en el que estés creanDo el GIFO';
-    start.classList.remove('hidden');
+    title.innerHTML = `¿Nos das acceso a tu cámara?`;
+    paragraph.innerHTML = `El acceso a tu camara será válido sólo <span class="breakpoint">por el tiempo en el que estés creando el GIFO</span>`;
+    start.classList.add('hidden');
+    circle[0].classList.toggle('active');
 }
 
 /**
@@ -40,10 +45,13 @@ async function getStreamAndRecord() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         videoGif(stream);
-        record.classList.add('hidden');
+        record.classList.remove('hidden');
+        const recorder = creatingRecorder(stream);
+        record.addEventListener('click', () => recording(recorder));
+        end.addEventListener('click', () => stopRecording(recorder));
     }
-    catch(e) {
-        console.log(e.message);
+    catch(error) {
+        console.log(error);
     }
 }
 
@@ -54,9 +62,113 @@ async function getStreamAndRecord() {
  */
 
 function videoGif(stream) {
-    video.classList.remove('hidden');
-    video.srcObject = stream;
-    video.play()
+  video.classList.remove('hidden');
+  video.srcObject = stream;
+  video.play()
+}
+
+/**
+ * @method creatingRecorder
+ * @param {} stream 
+ * @description - 
+ */
+
+function creatingRecorder(stream) {
+  const recorder = RecordRTC(stream, {
+    type: 'gif',
+    frameRate: 1,
+    quality: 10,
+    width: 360,
+    hidden: 240,
+    onGifRecordingStarted: function() {
+      console.log('started');
+    },
+  });
+  recorder.camera = stream;
+  return recorder;
+}
+
+/**
+ * @method recording
+ * @param {*} recorder
+ * @description - function that starts the recorder 
+ */
+
+function recording(recorder) {
+  timer.classList.remove("hidden");
+  startCount();
+  recorder.startRecording();
+  record.classList.add("hidden");
+  end.classList.remove("hidden");
+  circle[0].classList.toggle("active");
+  circle[1].classList.toggle("active");
+};
+
+/**
+ * @method recording
+ * @param {*} recorder
+ * @description - function that stop the recorder 
+ */
+
+function stopRecording(recorder) {
+  stopCount();
+  timer.classList.add('hidden');
+  end.classList.add('hidden');
+  up.classList.remove('hidden');
+  repeat.classList.remove('hidden');
+  recorder.stopRecording(() => {
+    let form = new FormData();
+    form.append('file', recorder.getBlob(), 'myGif.gif');
+    console.log(form.get('file'))
+  })
+  //video.srcObject = null;
+  recorder.camera.stop();
+  //recorder = null;
+}
+
+/**
+ * @method 
+ * @description - function that show the timer
+ */
+
+const time = numSeconds => {
+  let hours = Math.floor(numSeconds / 60 / 60);
+  numSeconds -= hours * 60 * 60;
+  let minutes = Math.floor(numSeconds / 60);
+  numSeconds -= minutes * 60;
+  numSeconds = parseInt(numSeconds);
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (numSeconds < 10) numSeconds = "0" + numSeconds;
+
+  return `${hours}:${minutes}:${numSeconds}`;
+};
+
+const updateTime = () => {
+  timer.textContent = time((Date.now() - initTime) / 1000);
+}
+
+// Ayudante para la duración; no ayuda en nada pero muestra algo informativo
+const startCount = () => {
+  initTime = Date.now();
+  idInterval = setInterval(updateTime, 1000);
+};
+
+const stopCount = () => {
+  clearInterval(idInterval);
+  initTime = null;
+  timer.textContent = "";
+}
+
+/**
+ * @method repeatVideoGif
+ * @description - function that repeat video
+ */
+
+function repeatVideoGif() {
+  repeat.classList.add('hidden');
+  up.classList.add('hidden');
+  getStreamAndRecord(); 
 }
 
 /**
@@ -64,3 +176,4 @@ function videoGif(stream) {
  */
 
 start.addEventListener('click', startVideo);
+repeat.addEventListener('click', repeatVideoGif);
