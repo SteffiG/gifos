2255632aedbbe8c955a50ba17108e8e5d910c5a8
Steffi Gutierrez <gutierrezsteffi@gmail.com>
2020-10-06 18:19:03 -0500

    menu

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,11 +1,13 @@
 /**
  * GLOBAL VARIABLES
  */
-
+/*
 const blackout = document.querySelector(".dark");
 const daylight = document.querySelector('.light');
-
+*/
 //CHANGE MODE
+
+const blackout = document.querySelector(".dark");
 
 /**
 * @method changeMode
@@ -15,25 +17,29 @@ const changeMode = (event) => {
     document.body.classList.toggle('oscuro');
     if(event.target.innerHTML === 'Modo Nocturno') {
         localStorage.setItem('darkMode', 'true');
-        blackout.classList.add('hidden');
-        daylight.classList.remove('hidden');
+        //blackout.classList.add('hidden');
+        //daylight.classList.remove('hidden');
+        document.getElementById('changeMode').innerHTML = 'Modo Diurno';
     } else {
         console.log('false');
         localStorage.setItem('darkMode', 'false');
-        blackout.classList.remove('hidden');
-        daylight.classList.add('hidden');
+        /*blackout.classList.remove('hidden');
+        daylight.classList.add('hidden');*/
+        document.getElementById('changeMode').innerHTML = 'Modo Nocturno';
     }
 };
 
 function verifyDarkMode() {
     if(localStorage.getItem('darkMode') === 'true') {
         document.body.classList.add('oscuro');
-        blackout.classList.add('hidden');
-        daylight.classList.remove('hidden');
+        /*blackout.classList.add('hidden');
+        daylight.classList.remove('hidden');*/
+        document.getElementById('changeMode').innerHTML = 'Modo Diurno'
     } else {
         document.body.classList.remove('oscuro');
-        blackout.classList.remove('hidden');
-        daylight.classList.add('hidden');
+        /*blackout.classList.remove('hidden');
+        daylight.classList.add('hidden');*/
+        document.getElementById('changeMode').innerHTML = 'Modo Nocturno';
     }
 }
 
